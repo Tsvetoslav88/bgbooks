522bbf27062c6fc7b6bcac7bd1577f1f1f45a2ac
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2017-11-26 15:39:22 +0200

    Added JpaRepository

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE BOOK (
   name varchar(200) NOT NULL,
   author varchar(100) NOT NULL,
+  type varchar(400),
   UNIQUE KEY `UK_book` (`name`, `author`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
