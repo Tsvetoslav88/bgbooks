e81b767cfcb56ac6d553a1eaaac2ae21360d0477
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2017-11-27 15:08:46 +0200

    Added EntityScan and JpaRepository

--- src/main/java/org/vexelon/net/bgbooks/BgbooksApplication.java
+++ src/main/java/org/vexelon/net/bgbooks/BgbooksApplication.java
@@ -3,12 +3,14 @@ package org.vexelon.net.bgbooks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("org.vexelon.net.bgbooks.repository")
+@EntityScan("org.vexelon.net.bgbooks.model")
 public class BgbooksApplication {
 
 	public static void main(String[] args) {
