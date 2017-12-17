8ea30e34ffb47f7fb02ded360df12328e6d0cd9b
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2017-12-17 21:07:27 +0200

    Added Component annotation in Repository class

--- src/main/java/org/vexelon/net/bgbooks/repository/BgBooksRepository.java
+++ src/main/java/org/vexelon/net/bgbooks/repository/BgBooksRepository.java
@@ -3,10 +3,12 @@ package org.vexelon.net.bgbooks.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.vexelon.net.bgbooks.model.Book;
 import org.vexelon.net.bgbooks.model.BookId;
 
+@Component("bgBooksRepository")
 @Repository
 public interface BgBooksRepository extends JpaRepository<Book, BookId>{
 	
